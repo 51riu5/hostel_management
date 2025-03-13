c2d59b101cd7f81a9a6fbb1b996870b84a7eba28
Rishit Menon <116945849+51riu5@users.noreply.github.com>
2025-03-13 11:36:09 +0530

    Update models.py
    added student general info table

--- hostelmanagement/users/models.py
+++ hostelmanagement/users/models.py
@@ -9,3 +9,19 @@ class User(models.Model):
     def __str__(self):
         return self.name
     
+class studentgeninfo(models.Model):
+    name = models.CharField(max_length=50)
+    rollno = models.CharField(max_length=50)
+    branch = models.CharField(max_length=50)
+    contactno = models.CharField(max_length=11)
+    yearofstudy = models.CharField(max_length=50)
+    dob = models.DateField()
+    bloodgroup=models.CharField(max_length=20)
+    nationality=models.CharField(max_length=50)
+    mothertongue=models.CharField(max_length=50)
+    aadhaar=models.CharField(max_length=50)
+    address=models.CharField(max_length=200)
+    
+
+    def __str__(self):
+        return self.name
